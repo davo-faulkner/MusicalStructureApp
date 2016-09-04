1bca3db169faf0152e99d5c2896684176be6e89c
Davo Faulkner <d@davo.co>
2016-09-03 20:02:15 -0500

    Add two more activities and add labels and parent attributes to manifest

---
+++ app/src/main/java/co/davo/musicalstructureapp/TopChartsActivity.java
@@ -0,0 +1,13 @@
+package co.davo.musicalstructureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopChartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_charts);
+    }
+}
