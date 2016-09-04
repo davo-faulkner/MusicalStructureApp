a49f61149ffc1f1ae983156f8f4ce118fc6a8907
Davo Faulkner <d@davo.co>
2016-09-03 20:46:43 -0500

    Add onClickListener and intent for Top Charts

--- app/src/main/java/co/davo/musicalstructureapp/MainActivity.java
+++ app/src/main/java/co/davo/musicalstructureapp/MainActivity.java
@@ -1,7 +1,11 @@
 package co.davo.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView topChartsTextView = (TextView) findViewById(R.id.top_charts_textView);
+
+        topChartsTextView.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent topChartsIntent = new Intent(MainActivity.this, TopChartsActivity.class);
+
+                startActivity(topChartsIntent);
+
+            }
+
+        });
     }
 }
+
